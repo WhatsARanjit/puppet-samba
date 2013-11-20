af330f1aabe79d427e48b1728e6a62f6ec0a0e67
Ranjit Viswakumar <ranjit@fedoranjit.co.sitespect.com>
2013-11-20 11:48:32 -0500

    Added the smb_user type, no provider yet

---
+++ lib/puppet/type/smb_user.rb
@@ -0,0 +1,16 @@
+Puppet::Type.newtype(:smb_user) do
+  desc 'The smb_user type manages usernames and passwords for samba shares'
+  ensurable
+  newparam(:user, :namevar => true) do
+    desc 'The user is the username of a samba share'
+    validate do |value|
+      fail("#{user} is not a valid username") unless value =~ /^[a-zA-Z0-9\-\_]+$/
+    end
+  end
+  newproperty(:password) do
+    desc 'The password is the user\'s password of a samba user'
+    validate do |value|
+      fail("Password should be at least 3 characters") unless value =~ /^...+$/
+    end
+  end
+end
